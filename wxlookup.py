a156f134d54ed7003fd2d027b55fd2516adeb605
David Sosby <dsosby@rim.com>
2012-03-13 09:49:33 -0500

    Works

--- wxlookup.py
+++ wxlookup.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 """A quick script that opens a webpage to my local weather"""
 
+import re
 import urllib
+import webbrowser
 
 def main():
-    wund_site = urllib.urlopen("http://wxunderground.com")
+    wund_site = urllib.urlopen("http://www.wunderground.com")
     wund_text = wund_site.read()
     wund_site.close()
 
-    print wund_text
+    local_url_re = re.compile(r'href="(/cgi-bin/findweather/getForecast\?query=\d+)')
+    local_url_m  = local_url_re.search(wund_text)
+    if local_url_m:
+        local_url = local_url_m.group(1)
+        webbrowser.open(r'http://www.wunderground.com%s' % local_url)
 
 if __name__ == "__main__":
     main()
